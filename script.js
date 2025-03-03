54cece8f726b9318e95ef518436e482f3059b0f4
theuzomavictor <siruzomav96@gmail.com>
2025-03-03 09:59:07 +0100

    Change default background color
    New dark background color makes portfolio softer in
    the eye on first view

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const iconEl = document.getElementById("mode-icon")
 const body = document.body
 
 function toggleEl() {
-    body.classList.toggle("dark-mode")
+    body.classList.toggle("light-mode")
 
-    if (body.classList.contains("dark-mode")) {
-        iconEl.classList.remove("fa-moon")
-        iconEl.classList.add("fa-sun")
-    } else {
+    if (body.classList.contains("light-mode")) {
         iconEl.classList.remove("fa-sun")
         iconEl.classList.add("fa-moon")
+    } else {
+        iconEl.classList.remove("fa-moon")
+        iconEl.classList.add("fa-sun")
     }
 }
