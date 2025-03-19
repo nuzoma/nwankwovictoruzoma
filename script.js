c56d2d0b384d4fe30f6a661b6cb5c560a083bd07
theuzomavictor <siruzomav96@gmail.com>
2025-03-19 09:31:22 +0100

    Add scroll feature to project images
    Project image scroll gives a neat representation of
    projects and allow users to get a holistic view of projects
    even before checking them out live.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const iconEl = document.getElementById("mode-icon")
+const iconEl = document.getElementById('mode-icon');
 const body = document.body
 
 function toggleEl() {
-    body.classList.toggle("light-mode")
+    body.classList.toggle('light-mode')
 
-    if (body.classList.contains("light-mode")) {
-        iconEl.classList.remove("fa-sun")
-        iconEl.classList.add("fa-moon")
+    if (body.classList.contains('light-mode')) {
+        iconEl.classList.remove('fa-sun')
+        iconEl.classList.add('fa-moon')
     } else {
-        iconEl.classList.remove("fa-moon")
-        iconEl.classList.add("fa-sun")
+        iconEl.classList.remove('fa-moon')
+        iconEl.classList.add('fa-sun')
     }
 }
